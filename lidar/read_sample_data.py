bd97ac626e04004290b8c2e353586f639fa45d97
Elson Lleshi <elson.lleshi-extern@renault.com>
2023-05-28 23:40:19 +0200

    read sample data

---
+++ lidar/read_sample_data.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def read_data():
+    with open("../resources/sample-data.txt", 'r') as data:
+        x = []
+        y = []
+        for line in data:
+            p = line.split(" ")
+            x.append(float(p[0]))
+            y.append(float(p[1]))
+    return x, y
+
+
+angle, distance = read_data()
+plt.scatter(np.array(angle), np.array(distance))
+plt.xlabel('X coordinate')
+plt.ylabel('Y coordinate')
+plt.title('LIDAR map from Fusion Team')
+plt.show()
